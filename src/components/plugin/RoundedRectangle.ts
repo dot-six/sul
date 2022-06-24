aaf030aa084a6940568b1f483f5174c8bd203971
Hanz <hanz@godot.id>
2022-06-24 08:54:45 +0700

    refactor: Fixed typing

--- src/components/plugin/RoundedRectangle.ts
+++ src/components/plugin/RoundedRectangle.ts
@@ -17,38 +17,8 @@ export class RoundedRectangle extends Geometry {
 	raw: ConvexShape = new ConvexShape(4);
 	radius: Radius;
 
-	constructor() {
-		this.radius = {
-			topleft: 0,
-			topright: 0,
-			bottomleft: 0,
-			bottomright: 0
-		};
-	}
-
-	constructor(radius: number) {
-		this.radius = {
-			topleft: radius,
-			topright: radius,
-			bottomleft: radius,
-			bottomright: radius
-		};
-	}
-
-	constructor(radius: Radius) {
-		// This is reference right??
-		this.radius = radius;
-	}
-
-	constructor(topleft: number, topright: number) {
-		this.radius = {
-			topleft, topright,
-			bottomleft: topleft,
-			bottomright: topright
-		};
-	}
-
-	constructor(topleft: number, topright: number, bottomleft: number, bottomright: number) {
+	constructor(topleft: number = 0, topright: number = 0, bottomleft: number = 0, bottomright: number = 0) {
+		super()
 		this.radius = {
 			topleft, topright,
 			bottomleft, bottomright
@@ -64,5 +34,7 @@ export class RoundedRectangle extends Geometry {
 		for (let i = 0; i < this.radius.topleft; i++ && pi++) {
 			this.raw.setPoint(pi, new Vector2D());
 		}
+
+		return [this.raw]
 	}
 };
